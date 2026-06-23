fe1bfab552ade7c4533246356b98802cb2c9e4c9
manya-gupta15 <manyagupta1510@gmail.com>
2026-06-24 00:21:42 +0530

    Implement zigzag level order traversal for binary tree

---
+++ week4/day2/zigzag.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+         vector <vector<int>> result;
+         if(root == NULL)
+         {
+            return result;
+         }
+         queue<TreeNode*> nodesQueue;
+         nodesQueue.push(root);
+         bool lefttoRight = true;
+         while(!nodesQueue.empty())
+         {
+            int size = nodesQueue.size();
+            vector<int> row(size);
+            for(int i=0;i<size;i++)
+            {
+                TreeNode * node = nodesQueue.front();
+                nodesQueue.pop();
+                int index =(lefttoRight)?i : (size-1-i);
+                row[index] = node -> val;
+                if(node ->left)
+                {
+                    nodesQueue.push(node->left);
+                }
+                if(node->right)
+                {
+                    nodesQueue.push(node->right);
+                }
+            }
+            lefttoRight = !lefttoRight;
+            result.push_back(row);
+         }
+         return result;
+    }
+};
