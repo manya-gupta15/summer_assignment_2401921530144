adaa31c50027e2d6f41c4934efd727b0382526bf
manya-gupta15 <manyagupta1510@gmail.com>
2026-06-26 22:04:34 +0530

    Create maxsum.cpp

---
+++ week4/day4/maxsum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int ans = INT_MIN;
+    int dfs(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        int left = max(0, dfs(root->left));
+        int right = max(0, dfs(root->right));
+        ans = max(ans, left + right + root->val);
+        return root->val + max(left, right);
+    }
+    int maxPathSum(TreeNode* root) {
+
+        dfs(root);
+        return ans;
+    }
+};
